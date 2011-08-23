574e4252b5a424e5d94708e5dbb087fc26db2604
Itay Neeman <itay@neeman.net>
2011-08-23 15:41:28 -0700

    Some more refactoring

--- platform/client/jquery_http.js
+++ platform/client/jquery_http.js
@@ -1,6 +1,20 @@
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 (function() {
-    var Splunk  = require('../splunk/splunk.js'),
-        __bind  = require('./utils').bind
+    var Splunk  = require('../../splunk').Splunk;
+    var utils   = Splunk.Utils;
 
     var root = exports || this;
 
@@ -11,7 +25,7 @@
                 type: message.method,
                 headers: message.headers,
                 data: message.body,
-                success: __bind(this, function (data, error, res) {
+                success: utils.bind(this, function (data, error, res) {
                     var response = {
                         statusCode: res.status
                     };
--- platform/node/node_http.js
+++ platform/node/node_http.js
@@ -1,7 +1,21 @@
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 (function() {
-    var request = require('request'),
-        Splunk  = require('../splunk/splunk.js'),
-        __bind  = require('./utils').bind
+    var request = require('request');
+    var Splunk  = require('../../splunk').Splunk;
+    var utils   = Splunk.Utils;
 
     var root = exports || this;
 
@@ -16,7 +30,7 @@
 
             console.log("URL: " + request_options.url);
 
-            request(request_options, __bind(this, function (error, res, data) {
+            request(request_options, utils.bind(this, function (error, res, data) {
                 var complete_response = this._buildResponse(error, res, data);
                 callback(complete_response);
             }));
