3889d1868d27b9f443c66ee7face26ec5757f625
Itay Neeman <itay@neeman.net>
2011-10-31 17:08:08 -0700

    Remove promises from client layer

--- tests/test_client.js
+++ tests/test_client.js
@@ -18,7 +18,6 @@ exports.run = (function() {
     var NodeHttp    = require('../platform/node/node_http').NodeHttp;
     var minitest    = require('../contrib/minitest');
     var utils       = Splunk.Utils;
-    var Promise     = Splunk.Promise;
     var Async       = Splunk.Async;
 
     var http = new NodeHttp();
@@ -38,7 +37,7 @@ exports.run = (function() {
     minitest.context("Job Tests", function() {
         this.setupContext(function(done) {
             var context = this;
-            svc.login(function(success) {
+            svc.login(function(err, success) {
                 context.service = svc;
                 context.success = success;
                 done();
@@ -51,44 +50,16 @@ exports.run = (function() {
             done();
         });
 
-        this.assertion("Promise#Create+cancel job", function(test) {
-            var sid = getNextId();
-            var jobP = this.service.jobs().create('search index=_internal | head 1', {id: sid});
-            jobP.when(
-                utils.bind(this, function(job) {   
-                    test.assert.ok(job);
-                    test.assert.strictEqual(job.sid, sid);
-
-                    var cancelP = job.cancel();
-                    cancelP.when(
-                        function() {
-                            test.finished();
-                        }
-                    );
-                })
-            );
-        });
-
         this.assertion("Callback#Create+cancel job", function(test) {
             var sid = getNextId();
-            this.service.jobs().create('search index=_internal | head 1', {id: sid}, utils.bind(this, function(job) {   
+            this.service.jobs().create('search index=_internal | head 1', {id: sid}, function(err, job) {   
                 test.assert.ok(job);
                 test.assert.strictEqual(job.sid, sid);
 
                 job.cancel(function() {
                     test.finished();
                 });
-            })); 
-        });
-
-        this.assertion("Promise#Create job error", function(test) {
-            var sid = getNextId();
-            var jobP = this.service.jobs().create('index=_internal | head 1', {id: sid});
-            jobP.whenFailed(
-                function() {
-                    test.finished();
-                }
-            );
+            }); 
         });
 
         this.assertion("Callback#Create job error", function(test) {
@@ -99,24 +70,8 @@ exports.run = (function() {
             });
         });
 
-        this.assertion("Promise#List jobs", function(test) {
-            var jobListP = this.service.jobs().list();
-            jobListP.when(
-                function(jobs) {
-                    test.assert.ok(jobs);
-                    test.assert.ok(jobs.length > 0);
-                
-                    for(var i = 0; i < jobs.length; i++) {
-                        test.assert.ok(jobs[i].isValid());
-                    }
-                
-                    test.finished();
-                }
-            );
-        });
-
         this.assertion("Callback#List jobs", function(test) {
-            this.service.jobs().list(function(jobs) {
+            this.service.jobs().list(function(err, jobs) {
                 test.assert.ok(jobs);
                 test.assert.ok(jobs.length > 0);
                 
@@ -128,91 +83,41 @@ exports.run = (function() {
             });
         });
 
-        this.assertion("Promise#Contains job", function(test) {
-            var sid = getNextId();
-
-            var jobP = this.service.jobs().create('search index=_internal | head 1', {id: sid});
-            var containsP = jobP.when(utils.bind(this, function(job) {   
-                test.assert.ok(job);
-                test.assert.strictEqual(job.sid, sid);
-                return Promise.join(job, this.service.jobs().contains(sid));
-            }));
-            var cancelP = containsP.when(function(job, contains) {
-                test.assert.ok(contains);
-                return job.cancel();
-            });
-            cancelP.when(function() {
-                test.finished(); 
-            });
-        });
-
         this.assertion("Callback#Contains job", function(test) {
+            var that = this;
             var sid = getNextId();
-            this.service.jobs().create('search index=_internal | head 1', {id: sid}, utils.bind(this, function(job) {   
+            this.service.jobs().create('search index=_internal | head 1', {id: sid}, function(err, job) {   
                 test.assert.ok(job);
                 test.assert.strictEqual(job.sid, sid);
 
-                this.service.jobs().contains(sid, function(contains) {
+                that.service.jobs().contains(sid, function(err, contains) {
                     test.assert.ok(contains);
 
                     job.cancel(function() {
                         test.finished();
                     });
                 });
-            })); 
-        });
-
-        this.assertion("Promise#job results", function(test) {
-            var sid = getNextId();
-            var service = this.service;
-            var job = null;
-
-            var jobP = this.service.jobs().create('search index=_internal | head 1 | stats count', {id: sid});
-            var doneP = jobP.when(function(createdJob) {
-                job = createdJob;
-                var properties = {};
-                return Promise.join(job, Promise.while({
-                    condition: function() { return properties.dispatchState !== "DONE"; },
-                    body: function() {
-                        return job.read().whenResolved(function(props) {
-                            properties = props;
-                            
-                            return Promise.sleep(1000);
-                        });
-                    }
-                }));
-            });
-            var resultsP = doneP.whenResolved(function(job) {
-                return job.results(); 
-            });
-            var finishedP = resultsP.whenResolved(function(results) {
-                test.assert.strictEqual(results.rows.length, 1);
-                test.assert.strictEqual(results.fields.length, 1);
-                test.assert.strictEqual(results.fields[0], "count");
-                test.assert.strictEqual(results.rows[0][0], "1");
-                
-                job.cancel().whenResolved(function() { test.finished(); });
-            });
+            }); 
         });
 
         this.assertion("Callback#job results", function(test) {
             var sid = getNextId();
             var service = this.service;
-            this.service.jobs().create('search index=_internal | head 1 | stats count', {id: sid}, function(job) {
+            this.service.jobs().create('search index=_internal | head 1 | stats count', {id: sid}, function(err, job) {
                 var properties = {};
 
-                Async.while(
+                Async.whilst(
                     {
                         condition: function() { return properties.dispatchState !== "DONE"; },
                         body: function(iterationDone) {
-                            job.read(function(props) {
+                            job.read(function(err, props) {
                                 properties = props;
                                 Async.sleep(1000, iterationDone); 
                             });
                         },
                     },
                     function() {
-                        job.results({}, function(results) {
+                        job.results({}, function(err, results) {
                             test.assert.strictEqual(results.rows.length, 1);
                             test.assert.strictEqual(results.fields.length, 1);
                             test.assert.strictEqual(results.fields[0], "count");
@@ -229,7 +134,7 @@ exports.run = (function() {
     minitest.context("App Tests", function() {
         this.setupContext(function(done) {
             var context = this;
-            svc.login(function(success) {
+            svc.login(function(err, success) {
                 context.service = svc;
                 context.success = success;
                 done();
@@ -242,50 +147,18 @@ exports.run = (function() {
             done();
         });
                
-        this.assertion("Promise#list applications", function(test) {
-            var apps = this.service.apps();
-            apps.list().whenResolved(function(appList) {
-                test.assert.ok(appList.length > 0);
-                test.finished();
-            });
-        });
-               
         this.assertion("Callback#list applications", function(test) {
             var apps = this.service.apps();
-            apps.list(function(appList) {
+            apps.list(function(err, appList) {
                 test.assert.ok(appList.length > 0);
-                test.finished();
-            });
-        });
-               
-        this.assertion("Promise#contains applications", function(test) {
-            var apps = this.service.apps();
-            apps.contains("search").whenResolved(function(found) {
-                test.assert.ok(found);
                 test.finished();
             });
         });
                
         this.assertion("Callback#contains applications", function(test) {
             var apps = this.service.apps();
-            apps.contains("search", function(found) {
+            apps.contains("search", function(err, found) {
                 test.assert.ok(found);
-                test.finished();
-            });
-        });
-        
-        this.assertion("Promise#create app", function(test) {
-            var name = "jssdk_testapp_" + getNextId();
-            var apps = this.service.apps();
-            
-            apps.create({name: name}).whenResolved(function(app) {
-                test.assert.ok(app.isValid());
-                var appName = app.properties().__name;
-                return Promise.join(app, apps.contains(appName));
-            }).whenResolved(function(app, found) {
-                test.assert.ok(found);
-                return app.del();
-            }).whenResolved(function() {
                 test.finished();
             });
         });
@@ -294,11 +167,12 @@ exports.run = (function() {
             var name = "jssdk_testapp_" + getNextId();
             var apps = this.service.apps();
             
-            apps.create({name: name}, function(app) {
+            apps.create({name: name}, function(err, app) {
                 test.assert.ok(app.isValid());
                 var appName = app.properties().__name;
-                apps.contains(appName, function(found) {
+                apps.contains(appName, function(err, found, entity) {
                     test.assert.ok(found);
+                    test.assert.ok(entity);
                     app.del(function() {
                         test.finished();
                     });
@@ -310,7 +184,7 @@ exports.run = (function() {
     minitest.context("Saved Search Tests", function() {
         this.setupContext(function(done) {
             var context = this;
-            svc.login(function(success) {
+            svc.login(function(err, success) {
                 context.service = svc;
                 context.success = success;
                 done();
@@ -323,9 +197,9 @@ exports.run = (function() {
             done();
         });
                
-        this.assertion("Promise#list", function(test) {
+        this.assertion("Callback#list", function(test) {
             var searches = this.service.savedSearches();
-            searches.list().whenResolved(function(savedSearches) {
+            searches.list(function(err, savedSearches) {
                 test.assert.ok(savedSearches.length > 0);
                 
                 for(var i = 0; i < savedSearches.length; i++) {
@@ -336,9 +210,9 @@ exports.run = (function() {
             });
         });
         
-        this.assertion("Promise#contains", function(test) {
+        this.assertion("Callback#contains", function(test) {
             var searches = this.service.savedSearches();
-            searches.contains("gentimes").whenResolved(function(found, search) {
+            searches.contains("gentimes", function(err, found, search) {
                 test.assert.ok(found);
                 test.assert.ok(search.isValid());
                 
@@ -346,29 +220,27 @@ exports.run = (function() {
             });
         });
         
-        this.assertion("Promise#history", function(test) {
+        this.assertion("Callback#history", function(test) {
             var searches = this.service.savedSearches();
-            searches.contains("gentimes").whenResolved(function(found, search) {
+            searches.contains("gentimes", function(err, found, search) {
                 test.assert.ok(found);
                 test.assert.ok(search.isValid());
                 
-                return search.history();
-            }).whenResolved(function(response) {
-                console.log(response.odata);
-                test.finished();
+                search.history(function(err, response) {
+                    test.finished();
+                });
             });
         });
         
-        this.assertion("Promise#suppress", function(test) {
+        this.assertion("Callback#suppress", function(test) {
             var searches = this.service.savedSearches();
-            searches.contains("gentimes").whenResolved(function(found, search) {
+            searches.contains("gentimes", function(err, found, search) {
                 test.assert.ok(found);
                 test.assert.ok(search.isValid());
                 
-                return search.suppressInfo();
-            }).whenResolved(function(response) {
-                console.log(response.odata);
-                test.finished();
+                search.suppressInfo(function(response) {
+                    test.finished();
+                });
             });
         });
     });
