42357a5b87bc2d3c76c0574c973cb07f9f5b0665
Itay Neeman <itay@neeman.net>
2011-11-14 15:42:22 -0800

    Preliminary switch to nodeunit
    Didn't quite work out, because it doesn't allow us to run in the
    browser either.

---
+++ browser.test.entry.js
@@ -0,0 +1,20 @@
+
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// This file is the entry point for client-side code, so it "exports" the
+// important functionality to the "window", such that others can easily
+// include it.
+
+window.SplunkTest = require('./splunk.test').SplunkTest;
