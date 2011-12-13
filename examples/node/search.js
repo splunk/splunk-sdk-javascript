64e703a6a967bc119ed7b96b19990cb7944b180c
Itay Neeman <itay@neeman.net>
2011-12-12 18:22:06 -0800

    Add search.js sample (no README or tests yet)

---
+++ examples/node/search.js
@@ -0,0 +1,199 @@
+
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+(function() {
+    var Splunk          = require('../../splunk').Splunk;
+    var Class           = Splunk.Class;
+    var utils           = Splunk.Utils;
+    var Async           = Splunk.Async;
+    var options         = require('../../internal/cmdline');
+    var print           = require('util').print;
+    
+    var FLAGS_CREATE = [
+        "search", "earliest_time", "latest_time", "now", "time_format",
+        "exec_mode", "search_mode", "rt_blocking", "rt_queue_size",
+        "rt_maxblocksecs", "rt_indexfilter", "id", "status_buckets",
+        "max_count", "max_time", "timeout", "auto_finalize_ec", "enable_lookups",
+        "reload_macros", "reduce_freq", "spawn_process", "required_field_list",
+        "rf", "auto_cancel", "auto_pause",
+    ];
+
+    var createService = function(options) {
+        return new Splunk.Client.Service({
+            scheme:     options.scheme,
+            host:       options.host,
+            port:       options.port,
+            username:   options.username,
+            password:   options.password
+        });
+    };
+    
+    var search = function(service, options, callback) {
+        // Extract the options we care about and delete them
+        // the object
+        var query = options.search;
+        var isVerbose = options.verbose;
+        var count = options.count || 0;
+        delete options.search;
+        delete options.verbose;
+        delete options.count;
+        
+        Async.chain([
+                // Create a search
+                function(done) {
+                    service.search(query, options, done);
+                },
+                // Poll until the search is complete
+                function(job, done) {
+                    Async.whilst(
+                        function() { return !job.properties().isDone; },
+                        function(iterationDone) {
+                            job.refresh(function(err, job) {
+                                if (err) {
+                                    callback(err);
+                                }
+                                else {
+                                    // If the user asked for verbose output,
+                                    // then write out the status of the search
+                                    var properties = job.properties();
+                                    if (isVerbose) {
+                                        var progress    = (properties.doneProgress * 100.0) + "%";
+                                        var scanned     = properties.scanCount;
+                                        var matched     = properties.eventCount;
+                                        var results     = properties.resultCount;
+                                        var stats = "-- " 
+                                            + progress + " done | " 
+                                            + scanned  + " scanned | " 
+                                            + matched  + " matched | "
+                                            + results  + " results"
+                                        print("\r" + stats + "                                          ");
+                                    }
+                                    
+                                    Async.sleep(1000, iterationDone);
+                                }
+                            })
+                        },
+                        function(err) {
+                            print("\r");
+                            done(err, job);
+                        }
+                    );
+                },
+                // Once the search is done, get the results
+                function(job, done) {
+                    job.results({count: count}, done);
+                },
+                // Print them out (as JSON), and cancel the job
+                function(results, job, done) {
+                    console.log(JSON.stringify(results));
+                    job.cancel(done);
+                }
+            ],
+            function(err) {
+                callback(err);
+            }
+        );
+    };
+    
+    var oneshotSearch = function(service, options, callback) {
+        var query = options.search;
+        delete options.search;
+        
+        // Oneshot searches don't have a job associated with them, so we
+        // simply execute it and print out the results.
+        service.oneshotSearch(query, options, function(err, results) {
+            if (err) {
+                callback(err);
+            }
+            else { 
+                console.log(JSON.stringify(results));
+                callback();
+            }
+        });
+    };
+
+    exports.main = function(argv, callback) {     
+        Splunk.Logger.setLevel("NONE");
+        
+        callback = callback || function(err) { 
+            if (err) {
+                console.log(err);
+            }
+            else {
+            }
+        };
+        var cmdline = options.create();
+        
+        cmdline.name = "search";
+        cmdline.description("Create a search and print the results to stdout");
+        cmdline.option("--verbose", "Output job progress as we wait for completion");
+        cmdline.option("--count <count>", "How many results to fetch");
+        
+        // For each of the flags, add an option to the parser
+        var flags = FLAGS_CREATE;
+        var required_flags = ["search"];
+        
+        for(var i = 0; i < flags.length; i++) {
+            var required = required_flags.indexOf(flags[i]) >= 0;
+            var option = "<" + flags[i] + ">";
+            cmdline.option("--" + flags[i] + " " + option, "", undefined, required);
+        }
+        
+        cmdline.on('--help', function(){
+            console.log("  Examples:");
+            console.log("  ");
+            console.log("  Create a regular search:");
+            console.log("  > node search.js --search 'search index=_internal | head 10'");
+            console.log("  ");
+            console.log("  Create a oneshot search:");
+            console.log("  > node search.js --search 'search index=_internal | head 10' --exec_mode oneshot");
+            console.log("  ");
+            console.log("  Create a regular search and only return 10 results:");
+            console.log("  > node search.js --search 'search index=_internal | head 2' --count 10");
+            console.log("  ");
+            console.log("  Create a regular search and output the progress while the search is running");
+            console.log("  > node search.js --search 'search index=_internal | head 2' --verbose");
+            console.log("  ");
+        });
+        
+        cmdline.parse(argv);
+        
+        var service = createService(cmdline.opts);
+        service.login(function(err, success) {
+            if (err || !success) {
+                callback("Error logging in");
+                return;
+            }
+            
+            delete cmdline.username;
+            delete cmdline.password;
+            delete cmdline.scheme;
+            delete cmdline.host;
+            delete cmdline.port;
+            delete cmdline.namespace;
+            
+            if (cmdline.opts.exec_mode === "oneshot") {
+                oneshotSearch(service, cmdline.opts, callback);
+            }
+            else {
+                search(service, cmdline.opts, callback);
+            }
+        });
+    };
+    
+    if (module === require.main) {
+        exports.main(process.argv);
+    }
+})();
