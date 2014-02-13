99a2cca7eb2d69165ac7bea9acc40e0ec4e5904b
Shakeel Mohamed <smohamed@splunk.com>
2014-02-13 12:36:25 -0800

    Finish alerts test suite

--- tests/test_service.js
+++ tests/test_service.js
@@ -1605,6 +1605,7 @@ exports.setup = function(svc, loggedOutSvc) {
             }
         },
         */
+        
         "Fired Alerts Tests": {
             setUp: function(done) {
                 this.service = svc;
@@ -1638,17 +1639,6 @@ exports.setup = function(svc, loggedOutSvc) {
                         function(search, done) {
                             test.ok(search);
                             test.strictEqual(search.alertCount(), 0);
-                            /*
-                            //TODO: test .firedAlerts().count()
-                            searches.fetch(function(err, savedSearches){
-                                var s = savedSearches.item("jssdk_savedsearch_alert_id0_1391658475250");
-                                //console.log(s.alertCount());
-                                //console.log(s.history());
-                                s.history(function(err, jobs, search){
-                                    console.log(jobs.length);
-                                });
-                            });
-                            */
                             search.history(done);
                         },
                         function(jobs, search, done) {
@@ -1676,22 +1666,22 @@ exports.setup = function(svc, loggedOutSvc) {
 
             "Callback#alert is triggered": function(test) {
                 var searches = this.service.savedSearches({owner: this.service.username});
-
+                var indexName = "sdk-tests-alerts";
                 var name = "jssdk_savedsearch_alert_" + getNextId();
+
+                // Real-time search config
                 var searchConfig = {
                     "name": name,
-                    "search": "index=_internal | head 1",
+                    "search": "index="+indexName+" | head 1",
                     "alert_type": "always",
                     "alert.severity": "2",
                     "alert.suppress": "0",
                     "alert.track": "1",
-                    "dispatch.earliest_time": "-1h",
-                    "dispatch.latest_time": "now",
+                    "dispatch.earliest_time": "rt-1m",
+                    "dispatch.latest_time": "rt",
                     "is_scheduled": "1",
                     "cron_schedule": "* * * * *"
                 };
-
-                var indexName = "sdk-tests-alerts";
 
                 Async.chain([
                         function(done) {
@@ -1721,39 +1711,11 @@ exports.setup = function(svc, loggedOutSvc) {
                         function(index, originalSearch, done) {
                             //Is the index enabled?
                             test.ok(!index.properties().disabled);
-
-                            //do a refresh
-                            /*
-                            test.ok(!index.properties().disabled); //make sure index is enabled
-
-                            //test the properties
-                            //sync==0
-                            test.strictEqual(index.properties().sync, 0);
-                            //disabled == false
-                            test.strictEqual(!index.properties().disabled);
-
                             //refresh the index
-
-                            //submit an an event
-
-                            //make sure event count is 1
-
-                            //do some kind of delay
-
-                            //check that event count is still 1
-
-                            //check that self.saved_search.fired_alerts == 1
-
-                            //clean it up: delete index and savedsearch, and alert?
-
-
-                            done(null, originalSearch);
-                            */
-
-                            //refresh the search
                             index.fetch(Async.augment(done, originalSearch));
                         },
                         function(index, originalSearch, done) {
+                            //Store the current event count for a later comparison 
                             var eventCount = index.properties().totalEventCount;
 
                             test.strictEqual(index.properties().sync, 0);
@@ -1777,30 +1739,45 @@ exports.setup = function(svc, loggedOutSvc) {
                         },
                         function(index, originalSearch, eventCount, done) {
                             // Did the event get submitted
-                            test.strictEqual(index.properties().totalEventCount, eventCount+1);
-
+                            test.strictEqual(index.properties().totalEventCount, eventCount+1);                            
+                            // Refresh the search
                             originalSearch.fetch(Async.augment(done, index));
-                            //TODO: try dispatching the savedSearch 
-                                //: also maybe see if you can poll the search every 1 second, etc
-                                //: or try doing a realtime search, making sure to delete it.
                         },
-                        function(originalSearch, done) {
-                            //Was an alert triggered? TODO: this function isn't working
-                                //I think the fetch isn't working actually.
-                            //test.strictEqual(originalSearch.alertCount(), 1);
-                            //Async.sleep(62000, Async.augment(done, originalSearch) );
-                            originalSearch.dispatch({"dispatch.now": true}, done);
+                        function(originalSearch, index, done) {
+                            console.log("\tAlert count pre-fetch", originalSearch.alertCount());
+                            var attemptNum = 1;
+                            var maxAttempts = 20;
+                            Async.whilst(
+                                function() {
+                                    //When this returns false, it hits the final function in the chain
+                                    console.log("\tFetch attempt", attemptNum, "alertCount", originalSearch.alertCount());
+                                    if(originalSearch.alertCount() != 0) {
+                                        return false;
+                                    }
+                                    else {
+                                        attemptNum++;
+                                        return attemptNum < maxAttempts;
+                                    }
+                                },
+                                function(callback) {
+                                    Async.sleep(500, function() { 
+                                        originalSearch.fetch(callback);
+                                    });
+                                },
+                                function(err) {
+                                    console.log("Attempted fetching", attemptNum, "of", maxAttempts, "result is", originalSearch.alertCount()!=0);
+                                    originalSearch.fetch(Async.augment(done, index));
+                                }
+                            );
                         },
-                        function(originalSearch, done) {
-                            //Was an alert triggered? TODO: this function isn't working
-                                //I think the fetch isn't working actually.
-                            //test.strictEqual(originalSearch.alertCount(), 1);
-                            console.log("alert count", originalSearch.alertCount());
-                            done(null, originalSearch);
+                        function(originalSearch, index, done) {
+                            // Make sure the event count has incremented, as expected
+                            test.strictEqual(originalSearch.alertCount(), 1);
+                            // Remove the search, especially because it's a real-time search
+                            originalSearch.remove(Async.augment(done, index));
                         },
-                        function(originalSearch, done) {
-                            //originalSearch.remove(done);
-                            done(null);
+                        function(index, done) {
+                            index.remove(done);
                         }
                     ],
                     function(err) {
@@ -1812,9 +1789,8 @@ exports.setup = function(svc, loggedOutSvc) {
                     }
                 );
             },
-
+            
             "Callback#delete all alerts": function(test) {
-                    //TODO: make this test work
                 var namePrefix = "jssdk_savedsearch_alert_";
                 var alertList = this.service.savedSearches().list();
 
