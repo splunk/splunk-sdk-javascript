a34ce74a9bccb1ea24aed874b6aee4540185d143
Shakeel Mohamed <smohamed@splunk.com>
2014-02-13 14:23:41 -0800

    Additional alert examples for create/delete

---
+++ examples/node/helloworld/firedalerts_create.js
@@ -0,0 +1,84 @@
+
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// This example will login to Splunk, and then create an alert.
+
+var splunkjs = require('../../../index');
+
+exports.main = function(opts, done) {
+    // This is just for testing - ignore it
+    opts = opts || {};
+    
+    var username = opts.username    || "admin";
+    var password = opts.password    || "1"; //TODO: revert to changeme
+    var scheme   = opts.scheme      || "https";
+    var host     = opts.host        || "localhost";
+    var port     = opts.port        || "8089";
+    var version  = opts.version     || "default";
+    
+    var service = new splunkjs.Service({
+        username: username,
+        password: password,
+        scheme: scheme,
+        host: host,
+        port: port,
+        version: version
+    });
+
+    // First, we log in
+    service.login(function(err, success) {
+        // We check for both errors in the connection as well
+        // as if the login itself failed.
+        if (err || !success) {
+            console.log("Error in logging in");
+            done(err || "Login failed");
+            return;
+        } 
+        
+        var alertOptions = {
+            name: "My Awesome Alert",
+            search: "index=_internal error sourcetype=splunkd* | head 10",
+            "alert_type": "always",
+            "alert.severity": "2",
+            "alert.suppress": "0",
+            "alert.track": "1",
+            "dispatch.earliest_time": "-1h",
+            "dispatch.latest_time": "now",
+            "is_scheduled": "1",
+            "cron_schedule": "* * * * *"
+        };
+        
+        // Now that we're logged in, Let's create a saved search
+        service.savedSearches().create(alertOptions, function(err, alert) {
+            if (err && err.status === 409) {
+                console.error("ERROR: A saved search with the name '" + alertOptions.name + "' already exists");
+                done();
+                return;
+            }
+            else if (err) {
+                console.error("There was an error creating the saved search:", err);
+                done(err);
+                return;
+            }
+            
+            console.log("Created saved search as alert: " + alert.name);            
+            done();
+        });
+    });
+};
+
+if (module === require.main) {
+    exports.main({}, function() {});
+}
---
+++ examples/node/helloworld/firedalerts_delete.js
@@ -0,0 +1,77 @@
+
+// Copyright 2011 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// This example will login to Splunk, and then try to remove the alert
+// that was created in savedsearches_create.js
+
+var splunkjs = require('../../../index');
+
+exports.main = function(opts, done) {
+    // This is just for testing - ignore it
+    opts = opts || {};
+    
+    var username = opts.username    || "admin";
+    var password = opts.password    || "1"; //TODO: revert to changeme
+    var scheme   = opts.scheme      || "https";
+    var host     = opts.host        || "localhost";
+    var port     = opts.port        || "8089";
+    var version  = opts.version     || "default";
+    
+    var service = new splunkjs.Service({
+        username: username,
+        password: password,
+        scheme: scheme,
+        host: host,
+        port: port,
+        version: version
+    });
+
+    // First, we log in
+    service.login(function(err, success) {
+        // We check for both errors in the connection as well
+        // as if the login itself failed.
+        if (err || !success) {
+            console.log("Error in logging in");
+            done(err || "Login failed");
+            return;
+        } 
+        
+        var name = "My Awesome Alert";
+        
+        // Now that we're logged in, let's delete the alert
+        service.savedSearches().fetch(function(err, firedAlerts) {
+            if (err) {
+                console.log("There was an error in fetching the alerts");
+                done(err);
+                return;
+            }
+
+            var alertToDelete = firedAlerts.item(name);
+            if (!alertToDelete) {
+                console.log("Can't delete '" + name + "' because it doesn't exist!");
+                done();
+            }
+            else {
+                alertToDelete.remove();
+                console.log("Deleted alert: " + name + "");
+                done();
+            }
+        });
+    });
+};
+
+if (module === require.main) {
+    exports.main({}, function() {});
+}
