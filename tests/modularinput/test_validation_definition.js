560dd81eb1ff3aa941924b4754dd282cbfb0463d
Shakeel Mohamed <smohamed@splunk.com>
2014-03-20 11:32:22 -0700

    Finish validation definition rough draft, add test for validation definition parse

---
+++ tests/modularinput/test_validation_definition.js
@@ -0,0 +1,68 @@
+
+// Copyright 2014 Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+exports.setup = function() {
+
+    var splunkjs                = require('../../index');
+    var modularinput            = splunkjs.ModularInput;
+    var ValidationDefinition    = modularinput.ValidationDefinition;
+    var fs                      = require("fs");
+
+    splunkjs.Logger.setLevel("ALL");
+    return {
+
+        "Moular Input tests": {
+            setUp: function(done) {
+                done();
+            },
+
+            "Validation Defintion#Parse produces expected result": function(test) {
+                var expected = {
+                    metadata: {
+                        "server_host": "tiny",
+                        "server_uri": "https://127.0.0.1:8089",
+                        "checkpoint_dir": "/opt/splunk/var/lib/splunk/modinputs",
+                        "session_key": "123102983109283019283",
+                        "name": "aaa"
+                    },
+                    parameters: {
+                        "param1": "value1",
+                        "param2": "value2",
+                        "disabled": "0",
+                        "index": "default",
+                        "multiValue": ["value1", "value2"],
+                        "multiValue2": ["value3", "value4"]
+                    }
+                };
+
+                var found = new ValidationDefinition();
+                found.parse(fs.readFileSync("data/validation.xml"));
+                test.ok(found.equals(expected));
+                test.done();
+            }
+        }
+    };
+};
+
+if (module === require.main) {
+    var splunkjs    = require('../../index');
+    var NodeHttp    = splunkjs.NodeHttp;
+    var test        = require('../../contrib/nodeunit/test_reporter');
+
+    var http = new NodeHttp();
+    
+    var suite = exports.setup(http);
+    test.run([{"Tests": suite}]);
+}
