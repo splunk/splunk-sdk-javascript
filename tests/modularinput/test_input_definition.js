44ed8c301a3c878afd126091f509b2dd7de01a48
Shakeel Mohamed <smohamed@splunk.com>
2014-03-20 12:59:20 -0700

    Add InputValidation class and tests

--- tests/modularinput/test_input_definition.js
+++ tests/modularinput/test_input_definition.js
@@ -15,39 +15,59 @@
 
 exports.setup = function() {
 
-    var splunkjs                = require('../../index');
-    var modularinput            = splunkjs.ModularInput;
-    var ValidationDefinition    = modularinput.ValidationDefinition;
-    var fs                      = require("fs");
+    var splunkjs           = require('../../index');
+    var modularinput       = splunkjs.ModularInput;
+    var InputDefinition    = modularinput.InputDefinition;
+    var fs                 = require("fs");
 
     splunkjs.Logger.setLevel("ALL");
     return {
 
-        "Validation Defintion tests": {
+        "Input Defintion tests": {
             setUp: function(done) {
                 done();
             },
 
-            "Parse produces expected result": function(test) {
-                var expected = new ValidationDefinition();
-                expected.metadata =  {
+            "Parse produces expected result - no inputs": function(test) {
+                var expected = new InputDefinition();
+                expected.metadata = {
                     "server_host": "tiny",
                     "server_uri": "https://127.0.0.1:8089",
-                    "checkpoint_dir": "/opt/splunk/var/lib/splunk/modinputs",
-                    "session_key": "123102983109283019283",
-                    "name": "aaa"
+                    "checkpoint_dir": "/some/dir",
+                    "session_key": "123102983109283019283"
                 };
-                expected.parameters = {
+
+                var found = new InputDefinition();
+                found.parse(fs.readFileSync("data/conf_with_0_inputs.xml"));
+                test.ok(found.equals(expected));
+                test.done();
+            },
+
+            "Parse produces expected result - 2 inputs": function(test) {
+                var expected = new InputDefinition();
+                expected.metadata = {
+                    "server_host": "tiny",
+                    "server_uri": "https://127.0.0.1:8089",
+                    "checkpoint_dir": "/some/dir",
+                    "session_key": "123102983109283019283"
+                };
+                expected.inputs["foobar://aaa"] = {
                     "param1": "value1",
                     "param2": "value2",
+                    "disabled": "0",
+                    "index": "default"
+                };
+                expected.inputs["foobar://bbb"] = {
+                    "param1": "value11",
+                    "param2": "value22",
                     "disabled": "0",
                     "index": "default",
                     "multiValue": ["value1", "value2"],
                     "multiValue2": ["value3", "value4"]
                 };
 
-                var found = new ValidationDefinition();
-                found.parse(fs.readFileSync("data/validation.xml"));
+                var found = new InputDefinition();
+                found.parse(fs.readFileSync("data/conf_with_2_inputs.xml"));
                 test.ok(found.equals(expected));
                 test.done();
             }
