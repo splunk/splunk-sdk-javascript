ff96920d4f5f66ec5ca243ab68a2c524e97cef97
Shakeel Mohamed <smohamed@splunk.com>
2014-09-04 13:15:26 -0700

    Update modinput examples to avoid non-public APIs

--- examples/modularinputs/github_commits/bin/app/github_commits.js
+++ examples/modularinputs/github_commits/bin/app/github_commits.js
@@ -218,7 +218,7 @@
                                 catch (e) {
                                     errorFound = true;
                                     working = false; // Stop streaming if we get an error.
-                                    Logger.error(name, e.message, eventWriter._err);
+                                    Logger.error(name, e.message);
                                     fs.appendFileSync(checkpointFilePath, checkpointFileNewContents); // Write to the checkpoint file
                                     done(e);
 
