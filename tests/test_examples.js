f6972f105a76b178121ae14f6c4dea422c2932f4
Shakeel Mohamed <smohamed@splunk.com>
2015-02-23 12:52:59 -0800

    Finish endpoint example

--- tests/test_examples.js
+++ tests/test_examples.js
@@ -62,6 +62,11 @@ exports.setup = function(svc, opts) {
                 var main = require("../examples/node/helloworld/firedalerts_delete").main;
                 main(opts, test.done);
             },
+
+            "Endpoint Instantiation": function(test) {
+                var main = require("../examples/node/helloworld/endpoint_instantiation").main;
+                main(opts, test.done);
+            },
             
             "Saved Searches": function(test) {
                 var main = require("../examples/node/helloworld/savedsearches").main;
