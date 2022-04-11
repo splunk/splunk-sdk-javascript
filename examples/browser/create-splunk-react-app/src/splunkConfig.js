6a011cd95e6d67dfcb7b6d6bc32e727b52bde801
akaila-splunk <akaila@splunk.com>
2022-04-11 13:20:21 +0530

    removed examples directory

--- examples/browser/create-splunk-react-app/src/splunkConfig.js
+++
@@ -1,6 +0,0 @@
-const splunkConfig = {
-    host: 'localhost',
-    port: '8089'
-};
-
-module.exports = { splunkConfig };
--- examples/modularinputs/random_numbers/bin/random_numbers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash  
-
-current_dir=$(dirname "$0")
-"$SPLUNK_HOME/bin/splunk" cmd node "$current_dir/app/random_numbers.js" $@
