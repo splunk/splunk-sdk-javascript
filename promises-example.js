be8175190a18c92561f330d616daccf9972901f2
akaila-splunk <akaila@splunk.com>
2022-05-19 15:08:48 +0530

    callbacks to promises code changes

---
+++ promises-example.js
@@ -0,0 +1,51 @@
+var splunkjs = require('./index');
+
+var token = "<Bearer_token/Session_key>";
+
+var serviceWithToken = new splunkjs.Service(
+    {
+        // Replace the host if you are accessing remote host
+        scheme: 'https',
+        host: 'localhost',
+        port: '8089',
+        sessionKey: token,
+        version: '8'
+    });
+
+function promise_example() {
+    var job = serviceWithToken.get("apps/local", { count: 5 });
+    
+    job.then((res) => {
+            console.log("Applications: ");
+            var appList = res.data.entry;
+            for(var i = 0; i < appList.length; i++){
+                var app = appList[i];
+                console.log("  App " + i + ": " + app.name);
+            }
+        }).catch((err) => {
+            console.log("There was an error retrieving the list of applications: ", err);
+        })
+}
+
+function abort_method_example() {
+    var callwithabort = function (promise, delay) {
+        return Promise.race([promise, promise.abort(delay)]);
+    } 
+    
+    var job = serviceWithToken.get("apps/local", { count: 5 });
+    
+    callwithabort(job, 10)
+    .then((res) => {
+            console.log("Applications:");
+            var appList = res.data.entry;
+            for(var i = 0; i < appList.length; i++){
+                var app = appList[i];
+                console.log("  App " + i + ": " + app.name);
+            }
+        }).catch((err) => {
+            console.log("There was an error retrieving the list of applications: ", err);
+        })
+}
+
+promise_example();
+// abort_method_example();
